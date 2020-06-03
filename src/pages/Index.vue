b1ecfa55b4bf1314e53f32f4ca6ba41800b745d7
Freddy <hey@frederik-ueberschaer.de>
2020-06-03 15:55:50 +0200

    Imported theme.json in Index.vue and display it below the Hero Section

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -2,6 +2,9 @@
   <Layout>
     <div class="container">
       <Hero />
+      <div class="" v-html= "settings.featured_posts">
+      </div>
+
       <ProjectsGrid :projects="$page.projects.edges" />
     </div>
     <LatestJournals :journals="$page.journals.edges" />
@@ -38,12 +41,18 @@ query Posts {
 import Hero from "@/components/Hero"
 import ProjectsGrid from "@/components/ProjectsGrid"
 import LatestJournals from "@/components/LatestJournals"
+import settings from "../../data/theme.json"
 
 export default {
   components: {
     Hero,
     ProjectsGrid,
     LatestJournals
+  },
+  data(){
+    return{
+      settings
+    }
   }
 }
 </script>
